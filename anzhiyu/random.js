4ac548ea4f6de195a7766ba99038dd1f65130687
azazel <2920857702@qq.com>
2025-05-16 18:02:12 +0800

    Site updated: 2025-05-16 18:02:11

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/05/12/Oracle常见概念/","2025/05/15/水平触发和边缘触发/","2025/05/10/数据管理/","2025/05/15/阻塞和非阻塞IO/"];function toRandomPost(){
+var posts=["2025/05/10/数据管理/","2025/05/12/Oracle常见概念/","2025/05/15/水平触发和边缘触发/","2025/05/15/阻塞和非阻塞IO/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
